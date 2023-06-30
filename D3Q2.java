9edffc2731d816f1ddc562c5613b9300acf7f0b6
GhoulAttack <EricHom123@gmail.com>
2023-06-30 20:22:02 +0800

    Update D3Q2.java

--- D3Q2.java
+++ D3Q2.java
@@ -63,21 +63,3 @@ class ListNode {
     }
 }
 
-/**
- * Your MyLinkedList object will be instantiated and called as such:
- * MyLinkedList obj = new MyLinkedList();
- * int param_1 = obj.get(index);
- * obj.addAtHead(val);
- * obj.addAtTail(val);
- * obj.addAtIndex(index,val);
- * obj.deleteAtIndex(index);
- */
-/**
- * Your MyLinkedList object will be instantiated and called as such:
- * MyLinkedList obj = new MyLinkedList();
- * int param_1 = obj.get(index);
- * obj.addAtHead(val);
- * obj.addAtTail(val);
- * obj.addAtIndex(index,val);
- * obj.deleteAtIndex(index);
- */
